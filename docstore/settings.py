9b3e49d3e62a38902a08f58f0020a296baa5040a
AminKipkeev2dot0 <aminkipkeev12@gmail.com>
2022-10-30 23:27:44 +0300

    created superuser,connected postgres,NOT made file upload

--- docstore/settings.py
+++ docstore/settings.py
@@ -15,6 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,8 +80,12 @@ WSGI_APPLICATION = 'docstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'postgres',
+    'USER': 'postgres',
+    'PASSWORD': '12345',
+    'HOST': '127.0.0.1',
+    'PORT': '5433'
     }
 }
 
@@ -106,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- personal_account/models.py
+++ personal_account/models.py
@@ -1,3 +1,14 @@
+from django import forms
+from django import forms
 from django.db import models
+from django.forms import FileField
+
+
+from docstore.settings import MEDIA_ROOT
+
 
 # Create your models here.
+class Doc(models.Model):
+    title=models.CharField(max_length=100)
+    descr=models.TextField()
+    scan=FileField(upload_to='scans')
