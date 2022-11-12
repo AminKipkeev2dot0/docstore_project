8c356d644c4715f0cd6ea7d1643b30d65e618195
AminKipkeev2dot0 <aminkipkeev12@gmail.com>
2022-11-13 02:13:28 +0300

    style of btn and side menu with docs

--- personal_account/admin.py
+++ personal_account/admin.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from .models import Doc
 class DocAdmin(admin.ModelAdmin):
     list_display=('title','descr','scan')
-    # list_display_links=('title','descr')
     search_fields=('title','descr')
 
 admin.site.register(Doc,DocAdmin)
